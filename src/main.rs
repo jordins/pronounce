24b317062bbd444c3714f042f8512733786f4ee6
jordins <jordi.nistal@gmail.com>
2021-03-13 19:56:11 +0100

    feat(audio): end program after sound finishes playing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use rodio::Source;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let text = "Hello%20jordi";
     let tl = "en";
-    // let url = "http://httpbin.org/range/26";
     let base_url = "https://translate.google.com/translate_tts?ie=UTF-8";
     let full_url = format!("{}&q={}&tl={}&client=tw-ob", base_url, text, tl);
     // println!("url {:?}", full_url);
@@ -14,8 +13,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cursor = Cursor::new(resp.bytes().unwrap());
     let source = rodio::Decoder::new(cursor).unwrap();
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    stream_handle.play_raw(source.convert_samples()).unwrap();
-
-    loop {}
-    // Ok(())
+    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+    sink.append(source);
+    sink.play();
+    sink.sleep_until_end();
+    Ok(())
 }
