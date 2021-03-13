d7872170a0268e50c29fbef5732392427f2160ab
jordins <jordi.nistal@gmail.com>
2021-03-13 20:26:51 +0100

    feat(cli): allow enter text by cli

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 use std::io::Cursor;
-
-use rodio::Source;
+extern crate clap;
+use clap::{App, Arg};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let text = "Hello%20jordi";
-    let tl = "en";
+    let matches = App::new("Pronounce")
+        .version("0.1.0")
+        .author("jordins <jordi.nistal@gmail.com>")
+        .about("Pronounces the passed text in the desired language")
+        .arg(
+            Arg::with_name("text")
+                .index(1)
+                .help("The text you want to pronounce")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("LANG")
+                .help("Sets the language to pronounce")
+                .default_value("en")
+                .index(2),
+        )
+        .get_matches();
+
+    let text = matches.value_of("text").unwrap();
+    println!("Value for text: {}", text);
+    let tl = matches.value_of("LANG").unwrap();
+    println!("Using lang: {}", tl);
+
     let base_url = "https://translate.google.com/translate_tts?ie=UTF-8";
     let full_url = format!("{}&q={}&tl={}&client=tw-ob", base_url, text, tl);
-    // println!("url {:?}", full_url);
 
     let resp = reqwest::blocking::get(&full_url)?;
     let cursor = Cursor::new(resp.bytes().unwrap());
